de8baa80077b0eb6f61c8c61c05a0b7def3e27ad
santiii05 <19221494@bsi.ac.id>
2023-10-27 14:06:10 +0700

    menambahkan

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Susantii</li>
+        <li>Nama Belakang : santi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> rusunawa penggilingan cakung jakarta timur</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>jawa tengah</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Basket</li>
+</ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <a href="http://www.RentalBuku.com">Susanti</a>
+        </footer>
+    </div>
+</body>
+</html>
